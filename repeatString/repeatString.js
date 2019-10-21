a54d12a1d642969053c79df5781c3599b6df6775
nguyeric <nguyeric@mail.gvsu.edu>
2019-10-21 16:57:55 -0400

    passed helloWorld and repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(stringRepeated, timesRepeated) {
+    let finalString = '';
+    if (timesRepeated < 0) return "ERROR";
+    for (i = 0; i < timesRepeated; i++) {
+        finalString += stringRepeated;
+    }
+    return finalString;
 }
 
 module.exports = repeatString
