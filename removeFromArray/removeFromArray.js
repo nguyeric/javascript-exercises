665ca7776a3e24f9a20f17250e883171c4bbb037
nguyeric <nguyeric@mail.gvsu.edu>
2019-10-22 10:45:00 -0400

    finished removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, remove) {
+    let newArr = arr;
+    for (i = 1; i < arguments.length; i++) {
+        for(j = 0; j < arr.length; j++) {
+            if (arr[j] == arguments[i]) newArr.splice(j,1);
+        }
+    }
+    return newArr;
 }
 
 module.exports = removeFromArray
