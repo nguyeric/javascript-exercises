207187b5fd857101007ad2cb0d2ef5777edd6ffb
nguyeric <nguyeric@mail.gvsu.edu>
2019-11-04 10:27:11 -0500

    passed all tests

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function() {
+const palindromes = function(string) {
+    /* let reverse = string.split("").reverse().join("");
+    console.log(reverse);
+    if (string == reverse) return true;
+    return false; */
 
+    let stringArr = string.split("");
+    let cleanString = [];
+    
+    stringArr.forEach(char => {
+        if (char.toLowerCase() != char.toUpperCase())
+            cleanString.push(char.toLowerCase());
+    })
+
+    return (cleanString.join("") == cleanString.reverse().join(""));
 }
 
 module.exports = palindromes
