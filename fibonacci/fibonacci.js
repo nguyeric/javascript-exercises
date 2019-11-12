4f34141364815f2d127cb71d0a973ad73b8d041e
nguyeric <nguyeric@mail.gvsu.edu>
2019-11-12 16:01:40 -0500

    passed all tests

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0)
+        return "OOPS";
+    else if (index == 0)
+        return 0;
 
+    let arr = [0, 1];
+
+    
+    for (let i = 2; i <= index; i++) {
+        arr = [arr[1], arr[0] + arr[1]];
+    }
+    return arr[1];
 }
 
 module.exports = fibonacci
