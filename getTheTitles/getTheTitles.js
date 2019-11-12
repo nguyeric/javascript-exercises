22c927f704960a7cc76faafdf3193b377fd90385
nguyeric <nguyeric@mail.gvsu.edu>
2019-11-12 16:07:18 -0500

    passed test

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+    /* let titles = [];
 
+    books.forEach(element => {
+        titles.push(element.title);
+    });
+
+    return titles; */
+
+    return books.map(element => element.title);
 }
 
 module.exports = getTheTitles;
